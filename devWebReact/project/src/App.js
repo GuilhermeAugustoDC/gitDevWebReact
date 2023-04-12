165b0d6a5ed9144e30846f433cdeedc3fb1ae3bd
Guilherme Augusto <guiidiasdc@gmail.com>
2023-04-12 08:52:33 -0300

    Alteração do nome da pasta.

---
+++ devWebReact/project/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import helloWorld from './components/HelloWorld';
+
+function App (){
+  return (
+    <div>
+      <helloWorld />
+    </div>
+  );
+}
+export default App;
---
+++ devWebReact/project/src/components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const helloWorld = () => {
+    return (
+        <div>
+            <h1>Hello world</h1>
+            <h2> Teste Return</h2>
+        </div>
+    );
+}
+
+export default helloWorld
